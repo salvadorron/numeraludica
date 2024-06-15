e7f08dff9ac0c6b2d11ea47f3713c511ecf9d2ea
Salvador Ron <salvadorronosio@gmail.com>
2024-06-15 00:01:19 -0400

    add token movement

--- src/components/Abacus/AbacusLevel.tsx
+++ src/components/Abacus/AbacusLevel.tsx
@@ -37,60 +37,10 @@ const abacusStyles: { [key: string]: CSSProperties } = {
 }
 export function AbacusLevel() {
 
-    // // Here get the Div that you want to follow the mouse
-    // const div_moving = document.getElementById('div_moving');
-
-    // // Here add the ID of the parent element
-    // const parent_div = 'parent_div';
-
-    // object to make a HTML element to follow mouse cursor ( https://coursesweb.net/ )
-    // const movingDiv = {
-    //     mouseXY: {},  // will contain the X, Y mouse coords inside its parent
-
-    //     // Get X and Y position of the elm (from: vishalsays.wordpress.com/ )
-    //     getXYpos: function (elm) {
-    //         let x = elm.offsetLeft;        // set x to elm’s offsetLeft
-    //         let y = elm.offsetTop;         // set y to elm’s offsetTop
-
-    //         elm = elm.offsetParent;    // set elm to its offsetParent
-
-    //         //use while loop to check if elm is null
-    //         // if not then add current elm’s offsetLeft to x, offsetTop to y and set elm to its offsetParent
-    //         while (elm != null) {
-    //             x = parseInt(x) + parseInt(elm.offsetLeft);
-    //             y = parseInt(y) + parseInt(elm.offsetTop);
-    //             elm = elm.offsetParent;
-    //         }
-
-    //         // returns an object with "xp" (Left), "=yp" (Top) position
-    //         return { 'xp': x, 'yp': y };
-    //     },
-
-    //     // Returns object with X, Y coords inside its parent
-    //     getCoords: function (e) {
-    //         const xy_pos = this.getXYpos(e.target);
-    //         const x = e.pageX;
-    //         const y = e.pageY;
-
-    //         x = x - xy_pos['xp'];
-    //         y = y - xy_pos['yp'];
-
-    //         return { 'xp': x, 'yp': y };
-    //     }
-    // };
-
-
-    // // registers 'mousemove' event to parent_div
-    // document.getElementById(parent_div).addEventListener('mousemove', function (e) {
-    //     mouseXY = movingDiv.getCoords(e);
-    //     div_moving.style.left = mouseXY.xp + 8 + 'px';
-    //     div_moving.style.top = mouseXY.yp - 8 + 'px';
-    // });
-
     const tokenGridItem = document.querySelectorAll<HTMLDivElement>('.token')
     let enterMouse = false;
     let activeElement = false;
-
+    let initX = 0;
 
     window.onmouseup = function (e) {
         activeElement = false;
@@ -104,11 +54,15 @@ export function AbacusLevel() {
 
         token.onmousedown = function (e) {
             activeElement = true;
+            initX = e.clientX;
         }
 
         token.onmousemove = function (e) {
-            if (enterMouse && activeElement && e.buttons === 1) {
-                token.style.left = (token.clientLeft) + 'px';
+            if (enterMouse && activeElement && e.buttons === 1) {          
+                let currentX = e.clientX
+                let x = currentX - initX
+                initX = currentX
+                token.style.left = x + (Number.parseInt(token.style.left.replace(/px/g, '')) || 0)  + 'px';
             }
         }
     })
